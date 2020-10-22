25b49a7c5593b141950bc3af12d4d3614404c668
Jasper Swallen <swallen@usc.edu>
2020-10-22 15:48:12 -0400

    start work on 2 different 4th order solutions
    right now, it seems like these solutions are wrong, so i'm just keeping track of my work

--- main.py
+++ main.py
@@ -58,41 +58,186 @@ print(
 its = int(input("Enter the number of iterations to perform: "))
 delta = float(input("Enter the δt factor (the length of each input): "))
 
-# declare arrays that will be appended to in loop
-x = [moving_mass_x]
-y = [moving_mass_y]
-x_prime = [moving_mass_x_prime]
-y_prime = [moving_mass_y_prime]
 
-# We have the following equations of motion
-# x ̇_(n+1)=x ̇_n-δt4π^2 (x_(n+1)/〖r_1^3〗_(n+1) +α(x_(n+1)-d)/〖r_2^3〗_(n+1) )
-# y ̇_(n+1)=y ̇_n-δt4π^2 y_(n+1) (1/〖r_1^3〗_(n+1) +α/〖r_2^3〗_(n+1) )
+def calculateXAccel(x_val, y_val):
+    return float(- 4 * (np.pi ** 2) * (x_val / np.power((x_val ** 2) + (y_val ** 2), 3 / 2) + alpha * (x_val - mass_2_x) / np.power(((x_val - mass_2_x) ** 2) + (y_val ** 2), 3 / 2)))
 
-for n in range(its):
-    # calculate next x, y, x', and y'
-    x_n_plus_one = x[n] + delta * x_prime[n]
-    x.append(x_n_plus_one)
-    y_n_plus_one = y[n] + delta * y_prime[n]
-    y.append(y_n_plus_one)
 
-    x_prime_n_plus_one = x_prime[n] - delta * 4 * (np.pi ** 2) * (x[n + 1] / np.power((x[n + 1] ** 2) + (
-        y[n + 1] ** 2), 3 / 2) + alpha * (x[n+1] - mass_2_x) / np.power((x[n + 1] - mass_2_x) ** 2 + y[n + 1] ** 2, 3 / 2))
-    x_prime.append(x_prime_n_plus_one)
+def calculateYAccel(x_val, y_val):
+    return float(- 4 * (np.pi ** 2) * y_val * (1 / np.power((x_val ** 2) + (y_val ** 2), 3 / 2) + 1 / np.power(((x_val - mass_2_x) ** 2) + (y_val ** 2), 3 / 2)))
 
-    y_prime_n_plus_one = y_prime[n] - delta * 4 * (np.pi ** 2) * y[n + 1] * (1 / np.power(
-        (x[n + 1] ** 2) + (y[n + 1] ** 2), 3 / 2) + 1 / np.power((x[n + 1] - mass_2_x) ** 2 + y[n + 1] ** 2, 3 / 2))
-    y_prime.append(y_prime_n_plus_one)
 
-plt.plot(x, y)
+def eulerMethod(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x, moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta):
+    # declare arrays that will be appended to in loop
+    x = [moving_mass_x]
+    y = [moving_mass_y]
+    x_prime = [moving_mass_x_prime]
+    y_prime = [moving_mass_y_prime]
 
-masses = [[mass_1_x, mass_2_x, moving_mass_x],
-          [mass_1_y, mass_2_y, moving_mass_y]]
-colors = np.array([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
+    # We have the following equations of motion
+    # x'_(n+1)=x'_n-deltat4pi^2 (x_(n+1)/(r_1^3)_(n+1) +alpha(x_(n+1)-d)/(r_2^3)_(n+1))
+    # y'_(n+1)=y'_n-deltat4pi^2 y_(n+1) (1/(r_1^3)_(n+1) +alpha/(r_2^3)_(n+1))
 
-plt.scatter(masses[0], masses[1], c=colors / 255)
+    for n in range(its):
+        # calculate next x, y, x', and y'
+        x_n_plus_one = x[n] + delta * x_prime[n]
+        x.append(x_n_plus_one)
+        y_n_plus_one = y[n] + delta * y_prime[n]
+        y.append(y_n_plus_one)
 
-labels = ["m1", "m2", "m"]
-for i, txt in enumerate(labels):
-    plt.annotate(txt, (masses[0][i], masses[1][i]))
+        x_prime_n_plus_one = x_prime[n] + delta * \
+            calculateXAccel(x[n + 1], y[n + 1])
+        x_prime.append(x_prime_n_plus_one)
 
-plt.show()
+        y_prime_n_plus_one = y_prime[n] + delta * \
+            calculateYAccel(x[n + 1], y[n + 1])
+        y_prime.append(y_prime_n_plus_one)
+
+    plt.plot(x, y)
+
+    masses = [[mass_1_x, mass_2_x, moving_mass_x],
+              [mass_1_y, mass_2_y, moving_mass_y]]
+    colors = np.array([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
+
+    plt.scatter(masses[0], masses[1], c=colors / 255)
+
+    labels = ["m1", "m2", "m"]
+    for i, txt in enumerate(labels):
+        plt.annotate(txt, (masses[0][i], masses[1][i]))
+
+    plt.show()
+
+
+def rungeKutta(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x, moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta):
+    # declare arrays that will be appended to in loop
+    x = [moving_mass_x]
+    y = [moving_mass_y]
+    x_prime = [moving_mass_x_prime]
+    y_prime = [moving_mass_y_prime]
+
+    # we have y_(n+1)=y_n+deltat/6(k_1+2k_2+2k_3+k_4)
+
+    for n in range(its):
+        # calculate next x, y, x', and y'
+        x_n_plus_one = x[n] + delta * x_prime[n]
+        x.append(x_n_plus_one)
+        y_n_plus_one = y[n] + delta * y_prime[n]
+        y.append(y_n_plus_one)
+
+        x_k_1 = delta * calculateXAccel(x[n + 1], y[n + 1])
+        y_k_1 = delta * calculateYAccel(x[n + 1], y[n + 1])
+
+        x_k_2 = delta * \
+            calculateXAccel(x[n + 1] + x_k_1 / 2, y[n + 1] + y_k_1 / 2)
+        y_k_2 = delta * \
+            calculateYAccel(x[n + 1] + x_k_1 / 2, y[n + 1] + y_k_1 / 2)
+
+        x_k_3 = delta * \
+            calculateXAccel(x[n + 1] + x_k_2 / 2, y[n + 1] + y_k_2 / 2)
+        y_k_3 = delta * \
+            calculateYAccel(x[n + 1] + x_k_2 / 2, y[n + 1] + y_k_2 / 2)
+
+        x_k_4 = delta * calculateXAccel(x[n + 1] + x_k_3, y[n + 1] + y_k_3)
+        y_k_4 = delta * calculateYAccel(x[n + 1] + x_k_3, y[n + 1] + y_k_3)
+
+        x_prime_n_plus_one = x_prime[n] + \
+            (x_k_1 + 2 * x_k_2 + 2 * x_k_3 + x_k_4) / 6
+        y_prime_n_plus_one = y_prime[n] + \
+            (y_k_1 + 2 * y_k_2 + 2 * y_k_3 + y_k_4) / 6
+
+        x_prime.append(x_prime_n_plus_one)
+        y_prime.append(y_prime_n_plus_one)
+
+    # print(x, y, x_prime, y_prime)
+
+    plt.plot(x, y)
+
+    masses = [[mass_1_x, mass_2_x, moving_mass_x],
+              [mass_1_y, mass_2_y, moving_mass_y]]
+    colors = np.array([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
+
+    plt.scatter(masses[0], masses[1], c=colors / 255)
+
+    labels = ["m1", "m2", "m"]
+    for i, txt in enumerate(labels):
+        plt.annotate(txt, (masses[0][i], masses[1][i]))
+
+    plt.show()
+
+
+def yoshida4thOrder(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x,
+                    moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta):
+    # declare arrays that will be appended to in loop
+    x = [moving_mass_x]
+    y = [moving_mass_y]
+    x_prime = [moving_mass_x_prime]
+    y_prime = [moving_mass_y_prime]
+
+    # Yoshida's 4th Order equation
+
+    # coefficients that stay the same each iteration
+    c_1 = 1 / (2 * (2 - np.power(2, 1 / 3)))
+    c_2 = (1 - np.power(2, 1 / 3)) / (2 * (2 - np.power(2, 1 / 3)))
+    c_3 = c_2
+    c_4 = c_1
+
+    d_1 = 1 / (2 - np.power(2, 1 / 3))
+    d_2 = -np.power(2, 1 / 3) / (2 * (2 - np.power(2, 1 / 3)))
+    d_3 = d_1
+    d_4 = 0
+
+    for n in range(its):
+        x_1 = x[n] + c_1 * x_prime[n] * delta
+        y_1 = y[n] + c_1 * y_prime[n] * delta
+
+        x_prime_1 = x_prime[n] + d_1 * calculateXAccel(x_1, y_1) * delta
+        y_prime_1 = y_prime[n] + d_1 * calculateYAccel(x_1, y_1) * delta
+
+        x_2 = x_1 + c_2 * x_prime_1 * delta
+        y_2 = y_1 + c_2 * y_prime_1 * delta
+
+        x_prime_2 = x_prime_1 + d_2 * calculateXAccel(x_2, y_2) * delta
+        y_prime_2 = y_prime_1 + d_2 * calculateYAccel(x_2, y_2) * delta
+
+        x_3 = x_2 + c_3 * x_prime_2 * delta
+        y_3 = y_2 + c_3 * y_prime_2 * delta
+
+        x_prime_3 = x_prime_2 + d_3 * calculateXAccel(x_3, y_3) * delta
+        y_prime_3 = y_prime_2 + d_3 * calculateYAccel(x_3, y_3) * delta
+
+        x_4 = x_3 + c_4 * x_prime_3 * delta
+        y_4 = y_3 + c_4 * y_prime_3 * delta
+
+        x_prime_4 = x_prime_3 + d_4 * calculateXAccel(x_4, y_4) * delta
+        y_prime_4 = y_prime_3 + d_4 * calculateYAccel(x_4, y_4) * delta
+
+        x.append(x_4)
+        y.append(y_4)
+        x_prime.append(x_prime_4)
+        y_prime.append(y_prime_4)
+
+    # print(x, y, x_prime, y_prime)
+
+    plt.plot(x, y)
+
+    masses = [[mass_1_x, mass_2_x, moving_mass_x],
+              [mass_1_y, mass_2_y, moving_mass_y]]
+    colors = np.array([[255, 0, 0], [0, 255, 0], [0, 0, 255]])
+
+    plt.scatter(masses[0], masses[1], c=colors / 255)
+
+    labels = ["m1", "m2", "m"]
+    for i, txt in enumerate(labels):
+        plt.annotate(txt, (masses[0][i], masses[1][i]))
+
+    plt.show()
+
+
+eulerMethod(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x,
+            moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta)
+
+rungeKutta(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x,
+           moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta)
+
+yoshida4thOrder(mass_1_x, mass_1_y, mass_2_x, mass_2_y, alpha, moving_mass_x,
+                moving_mass_y, moving_mass_x_prime, moving_mass_y_prime, its, delta)
